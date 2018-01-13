a7d34678bae0bce7c084cd117558a5ec61a8cb29
uni-lovenix <loveyanga@qq.com>
2018-01-13 14:54:03 +0800

    add jieba

--- chongding.py
+++ chongding.py
@@ -7,6 +7,7 @@ import pytesseract
 import re
 import webbrowser
 import urllib
+import jieba
 
 r = re.compile(r'[0-9]\.*.?')
 
@@ -18,6 +19,16 @@ if not os.path.isdir(screenshot_backup_dir):
 
 def pull_screenshot():
     c.screenshot('1.png')
+
+def baidu_search(word):
+	p = {'wd':word}
+	url = 'http://www.baidu.com/s?' + urllib.parse.urlencode(p)
+	webbrowser.open(url)
+
+def google_search(word):
+	p_g = {'q':word}
+	url_g = 'https://www.google.co.jp/search?' + urllib.parse.urlencode(p_g)
+	webbrowser.open(url_g)
 
 while(True):
 	q = input('input:')
@@ -32,17 +43,25 @@ while(True):
 	    s = l[i]
 	    s = ''.join(s.split('\n'))
 	    l[i] = s
-	print(l)
+	# print(l)
 
 	word = ''
 	for s in l:
 	    g = r.match(s)
 	    if g != None:
-	        print(s)
+	        # print(s)
 	        word = s
 	        break
-	p = {'wd':word}
-	url = 'http://www.baidu.com/s?' + urllib.parse.urlencode(p)
-	webbrowser.open(url)
+
+	cuts = jieba.cut(word, cut_all=False)
+	w = ''
+	for i  in cuts:
+	    # print(c)
+	    w += i +' '
+	word = w
+
+	baidu_search(word)
+	# google_search(word)
+
 	end = time.time()
 	print('using time:', end-start)
