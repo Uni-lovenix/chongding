fa766886f9e42143417d9949a27aa52f55ae217a
uni-lovenix <loveyanga@qq.com>
2018-01-16 00:29:26 +0800

    change file.

--- chongding.py
+++ chongding.py
@@ -1,27 +1,26 @@
 # coding=utf-8
 import os
 import time
+import re
+import shutil
 from PIL import Image
 import wda
 import pytesseract
-import re
 import webbrowser
 import urllib
 import jieba
 
+
+i = 1
 isusingjieba = False
-
 r = re.compile(r'[0-9]\.*.?')
-
 c = wda.Client()
 s = c.session()
 screenshot_backup_dir = 'screenshot_backups/'
 if not os.path.isdir(screenshot_backup_dir):
     os.mkdir(screenshot_backup_dir)
 
-def pull_screenshot():
-    c.screenshot('1.png')
-
+    
 def baidu_search(word):
 	p = {'wd':word}
 	url = 'http://www.baidu.com/s?' + urllib.parse.urlencode(p)
@@ -40,35 +39,44 @@ def usingjieba(word):
 	    w += i +' '
 	return word
 
+def bakpng():
+	name = time.time()
+	shutil.copyfile('./1.png', './screenshot_backups/'+name+'.png')
 
-while(True):
-	q = input('input:')
-	start = time.time()
-	if q.startswith('q'):
-		break
-	pull_screenshot()
-	s = pytesseract.image_to_string(Image.open('./1.png'), lang='chi_sim')
-	print(s)
-	l = s.split('\n\n')
-	for i in range(len(l)):
-	    s = l[i]
-	    s = ''.join(s.split('\n'))
-	    l[i] = s
-	# print(l)
 
-	word = ''
-	for s in l:
-	    g = r.match(s)
-	    if g != None:
-	        # print(s)
-	        word = s
-	        break
+def main():
+	while(True):
+		q = input('input:')
+		start = time.time()
+		if q.startswith('q'):
+			break
+		c.screenshot('1.png')
+		s = pytesseract.image_to_string(Image.open('1.png'), lang='chi_sim')
+		print(s)
+		l = s.split('\n\n')
+		for i in range(len(l)):
+		    s = l[i]
+		    s = ''.join(s.split('\n'))
+		    l[i] = s
+		# print(l)
 
-	if isusingjieba:
-		word = usingjieba(word)
+		word = ''
+		for i in range(len(l)):
+		    g = r.match(l[i])
+		    if g != None:
+		        # print(l[i])
+		        word = l[i] + l[i+1]
+		        break
 
-	baidu_search(word)
-	# google_search(word)
+		if isusingjieba:
+			word = usingjieba(word)
 
-	end = time.time()
-	print('using time:', end-start)
+		baidu_search(word)
+		# google_search(word)
+
+		end = time.time()
+		print('using time:', end-start)
+		bakpng()
+
+if __name__ == '__main__':
+	main()
